e9b4646f47b822080c37d6f3e6b446a323285ba8
rstijerina <r.sal.tijerina@gmail.com>
2022-09-22 12:01:57 -0500

    update hello world

--- applications/hello-world/run.sh
+++ applications/hello-world/run.sh
@@ -1,9 +1,10 @@
 FULL_GREETING="${Greeting} ${Target}. My name is ${_tapisJobOwner}"
 echo "$FULL_GREETING"
 
-if [ -e $"fileToModify" ]
+if [ -e "$fileToModify" ]
 then
     echo $FULL_GREETING >> $fileToModify
+    cp $fileToModify /TapisOutput
 else
-    echo $FULL_GREETING > out.txt
+    echo $FULL_GREETING > /TapisOutput/out.txt
 fi
