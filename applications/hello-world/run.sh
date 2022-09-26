1700b329cf53346ce34e01f8b22b1efdee5872a8
rstijerina <r.sal.tijerina@gmail.com>
2022-09-26 17:26:02 -0500

    fix app args

--- applications/hello-world/run.sh
+++ applications/hello-world/run.sh
@@ -1,10 +1,19 @@
+#!/bin/bash
+echo Your container args are: "$@"
+
+Greeting=$1
+Target=$2
+
 FULL_GREETING="${Greeting} ${Target}. My name is ${_tapisJobOwner}"
 echo "$FULL_GREETING"
+echo `pwd`
+
+fileToModify=$_tapisExecSystemInputDir/in.txt
 
 if [ -e "$fileToModify" ]
 then
     echo $FULL_GREETING >> $fileToModify
-    cp $fileToModify /TapisOutput
+    cp $fileToModify $_tapisExecSystemOutputDir
 else
-    echo $FULL_GREETING > /TapisOutput/out.txt
+    echo $FULL_GREETING > $_tapisExecSystemOutputDir/out.txt
 fi
