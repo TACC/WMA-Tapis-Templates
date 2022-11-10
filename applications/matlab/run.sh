98fe9857c3a88aae44957173f296cc18508b5a67
rstijerina <r.sal.tijerina@gmail.com>
2022-11-10 14:36:42 -0600

    add test matlab app with license

---
+++ applications/matlab/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "Your command line args (appArgs) are: $@"
+
+Greeting=$1
+Target=$2
+
+FULL_GREETING="${Greeting} ${Target}. My name is ${_tapisJobOwner}"
+echo "$FULL_GREETING"
+echo `pwd`
+
+fileToModify=$_tapisExecSystemInputDir/in.txt
+
+if [ -e "$fileToModify" ]
+then
+    echo $FULL_GREETING >> $fileToModify
+    cp $fileToModify $_tapisExecSystemOutputDir
+else
+    echo $FULL_GREETING > $_tapisExecSystemOutputDir/out.txt
+fi
+
+# set up license file
+cat << EOT >> $_tapisExecSystemOutputDir/.app_license
+${_license}
+EOT
