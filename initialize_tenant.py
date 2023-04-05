c575d4f19cb0855d9d2a060d84d90adaac648089
rstijerina <r.sal.tijerina@gmail.com>
2023-04-05 17:13:18 -0500

    rename singularity profile; add category field to notes; move portalName to tags

--- initialize_tenant.py
+++ initialize_tenant.py
@@ -1,26 +1,12 @@
 from tapipy.tapis import Tapis
 from tapipy.errors import BaseTapyException
 import os
-import client_secrets
+from client_secrets import TENANT_NAMES, CLIENT_USERNAME, CLIENT_PASSWORD
 from utils.load_file_to_json import load_file_to_json
 
-TENANT_NAME = 'CEP'
 
-match TENANT_NAME:
-    case 'A2CPS':
-        TENANT_BASE_URLS = ['https://a2cps.tapis.io',
-                            'https://a2cps.develop.tapis.io']
-        SYSTEMS = ['secure.frontera', 'secure.cloud.corral']
-        APPS = ['a2cps/extract-secure', 'a2cps/compress-secure',
-                'a2cps/matlab-secure', 'a2cps/rstudio-desktop-secure']
-    case _:
-        TENANT_BASE_URLS = ['https://portals.develop.tapis.io']
-        SYSTEMS = ['frontera', 'maverick2', 'ls6', 'cloud.data']
-        APPS = ['compress', 'extract', 'matlab', 'rstudio-desktop']
-
-
-def provision(client):
-    profile = load_file_to_json('systems/tacc-singularity.json')
+def provision(client, systems, apps):
+    profile = load_file_to_json('systems/tacc_singularity.json')
     try:
         client.systems.createSchedulerProfile(**profile)
         print('profile created: {}'.format(profile['name']))
@@ -28,7 +14,7 @@ def provision(client):
         print('profile already exists: {}'.format(profile['name']))
         pass
 
-    for system in SYSTEMS:
+    for system in systems:
         sys_json = load_file_to_json(f'systems/{system}.json')
         try:
             client.systems.createSystem(**sys_json)
@@ -40,7 +26,7 @@ def provision(client):
         client.files.sharePathPublic(
             systemId=sys_json['id'], path=sys_json['rootDir'])
 
-    for app_name in APPS:
+    for app_name in apps:
         app_json = load_file_to_json(f'applications/{app_name}/app.json')
         if os.path.isfile(f'applications/{app_name}/profile.json'):
             profile = load_file_to_json(
@@ -63,12 +49,26 @@ def provision(client):
 
 
 def main():
-    for tenant_base_url in TENANT_BASE_URLS:
-        print(f'provisioning tenant: {tenant_base_url}')
-        client = Tapis(base_url=tenant_base_url, username=client_secrets.CLIENT_USERNAME,
-                       password=client_secrets.CLIENT_PASSWORD, resource_set="dev", download_latest_specs=True)
-        client.get_tokens()
-        provision(client)
+    for tenant_name in TENANT_NAMES:
+        match tenant_name:
+            case 'A2CPS':
+                tenant_base_urls = ['https://a2cps.tapis.io',
+                                    'https://a2cps.develop.tapis.io']
+                systems = ['secure.frontera', 'secure.cloud.corral']
+                apps = ['a2cps/extract-secure', 'a2cps/compress-secure',
+                        'a2cps/matlab-secure', 'a2cps/rstudio-desktop-secure']
+            case _:
+                tenant_base_urls = ['https://portals.develop.tapis.io']
+                systems = ['frontera', 'maverick2', 'ls6', 'cloud.data']
+                apps = ['compress', 'extract', 'matlab',
+                        'rstudio-desktop', 'jupyter-notebook-hpc', 'hello-world']
+
+        for tenant_base_url in tenant_base_urls:
+            print(f'provisioning tenant: {tenant_base_url}')
+            client = Tapis(base_url=tenant_base_url, username=CLIENT_USERNAME,
+                           password=CLIENT_PASSWORD, resource_set="dev", download_latest_specs=True)
+            client.get_tokens()
+            provision(client, systems, apps)
 
 
 if __name__ == "__main__":
