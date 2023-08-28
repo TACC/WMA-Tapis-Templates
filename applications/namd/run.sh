47c4117e1d2515b8727d6924922c55da22527ff1
Juan Martinez <jmartinez@tacc.utexas.edu>
2023-08-28 15:14:32 -0500

    NAMD update.

--- applications/namd/run.sh
+++
@@ -1,7 +0,0 @@
-set -x
-${AGAVE_JOB_CALLBACK_RUNNING}
-echo "confFile is ${confFile}"
-echo "namdCommandLineOptions is ${namdCommandLineOptions}"
-echo "namd"
-cd "${inputDirectory}"
-ibrun namd2 ${namdCommandLineOptions} ${confFile}
