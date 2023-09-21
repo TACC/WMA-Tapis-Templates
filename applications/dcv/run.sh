c9b2d22ff0c8b5fa33f7b1cf9b44d316a450e817
Sal Tijerina <r.sal.tijerina@gmail.com>
2023-09-21 17:18:49 -0500

    dcv progress

---
+++ applications/dcv/run.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+set -x
+
+echo "TACC: job $SLURM_JOB_ID execution at: `date`"
+
+# our node name
+NODE_HOSTNAME=`hostname -s`
+
+# HPC system target. Used as DCV host
+HPC_HOST=`hostname -d`
+
+echo "TACC: running on node $NODE_HOSTNAME on $HPC_HOST"
+
+TAP_FUNCTIONS="/share/doc/slurm/tap_functions"
+if [ -f ${TAP_FUNCTIONS} ]; then
+    . ${TAP_FUNCTIONS}
+else
+    echo "TACC:"
+    echo "TACC: ERROR - could not find TAP functions file: ${TAP_FUNCTIONS}"
+    echo "TACC: ERROR - Please submit a consulting ticket at the TACC user portal"
+    echo "TACC: ERROR - https://portal.tacc.utexas.edu/tacc-consulting/-/consult/tickets/create"
+    echo "TACC:"
+    echo "TACC: job $SLURM_JOB_ID execution finished at: $(date)"
+    exit 1
+fi
+
+# confirm DCV server is alive
+DCV_SERVER_UP=`systemctl is-active dcvserver`
+if [ $DCV_SERVER_UP != "active" ]; then
+    echo "TACC:"
+    echo "TACC: ERROR - could not confirm dcvserver active, 'systemctl is-active dcvserver' returned '$DCV_SERVER_UP'"
+    exit 1
+fi
+
+# if X0 socket exists, then DCV will use a higher X display number and ruin our day
+# therefore, cowardly bail out and appeal to an admin to fix the problem
+if [ -f /tmp/.X11-unix/X0 ]; then
+    echo "TACC:"
+    echo "TACC: ERROR - X0 socket already exists. DCV script will fail."
+    echo "TACC: ERROR - Please submit a consulting ticket at the TACC user portal"
+    echo "TACC: ERROR - https://portal.tacc.utexas.edu/tacc-consulting/-/consult/tickets/create"
+    echo "TACC:"
+    exit 1
+fi
+
+# create an X startup file in /tmp
+# source xinitrc-common to ensure xterms can be made
+# then source the user's xstartup if it exists
+XSTARTUP="/tmp/dcv-startup-$USER"
+cat <<- EOF > $XSTARTUP
+#!/bin/sh
+
+unset SESSION_MANAGER
+unset DBUS_SESSION_BUS_ADDRESS
+. /etc/X11/xinit/xinitrc-common
+exec startxfce4
+EOF
+
+chmod a+rx $XSTARTUP
+
+# create DCV session for this job
+DCV_HANDLE="${_tapisJobUUID}-session"
+dcv create-session --owner ${_tapisJobOwner} --init=$XSTARTUP $DCV_HANDLE
+
+# Wait a few seconds for dcvserver to spin up
+sleep 3;
+
+if ! `dcv list-sessions 2>&1 | grep -q ${DCV_HANDLE}`; then
+    echo "TACC:"
+    echo "TACC: WARNING - could not find a DCV session for this job"
+    echo "TACC: WARNING - This could be because all DCV licenses are in use."
+    echo "TACC: WARNING - Failing over to VNC session."
+    echo "TACC: "
+    echo "TACC: If you rarely receive a DCV session using this script, "
+    echo "TACC: please submit a consulting ticket at the TACC user portal:"
+    echo "TACC: https://portal.tacc.utexas.edu/tacc-consulting/-/consult/tickets/create"
+    echo "TACC: "
+
+    exit 1
+else
+    LOCAL_PORT=8443  # default DCV port
+    DISPLAY=":0"
+fi
+
+echo "TACC: local (compute node) DCV port is $LOCAL_PORT"
+
+LOGIN_PORT=$(tap_get_port)
+echo "TACC: got login node DCV port $LOGIN_PORT"
+
+# Wait a few seconds for good measure for the job status to update
+sleep 3;
+
+# create reverse tunnel port to login nodes.  Make one tunnel for each login so the user can just connect to $HPC_HOST
+for i in `seq 4`; do
+  ssh -o StrictHostKeyChecking=no -f -g -N -R $LOGIN_PORT:$NODE_HOSTNAME:$LOCAL_PORT login$i
+done
+
+echo "TACC: Created reverse ports on $HPC_HOST logins"
+echo "TACC:          https://$HPC_HOST:$LOGIN_PORT"
+
+
+# Webhook callback url for job ready notification.
+# Notification is sent to _INTERACTIVE_WEBHOOK_URL, e.g. https://3dem.org/webhooks/interactive/
+INTERACTIVE_SESSION_ADDRESS="https://${HPC_HOST}:${LOGIN_PORT}"
+curl -k --data "event_type=interactive_session_ready&address=${INTERACTIVE_SESSION_ADDRESS}&owner=${_tapisJobOwner}&job_uuid=${_tapisJobUUID}" "${_INTERACTIVE_WEBHOOK_URL}" &
+
+# Run an xterm and launch $_XTERM_CMD for the user; execution will hold here.
+export DISPLAY
+xterm -r -ls -geometry 80x24+10+10 -title '*** Exit this window to kill your interactive session ***' -e "${_XTERM_CMD}"
+
+# Job is done!
+
+echo "TACC: closing DCV session"
+
+dcv close-session ${DCV_HANDLE}
+
+# wait a brief moment so dcv can clean up after itself
+sleep 1
+
+echo "TACC: release port returned $(tap_release_port ${LOGIN_PORT} 2> /dev/null)"
+
+# remove X11 sockets so DCV will find :0 next time
+find /tmp/.X11-unix -user $USER -exec rm -f '{}' \;
+
+echo "TACC: job $SLURM_JOB_ID execution finished at: `date`"
