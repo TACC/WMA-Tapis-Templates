bc3b9fc2bef3ccee2c979d76f7155e74c933a9bf
Sal Tijerina <r.sal.tijerina@gmail.com>
2023-11-30 18:38:13 -0600

    update jupyter lab to be host-agnostic; clean up script; add ls6 version

--- applications/jupyter-lab-hpc/run.sh
+++ applications/jupyter-lab-hpc/run.sh
@@ -2,19 +2,8 @@
 
 echo "TACC: job ${SLURM_JOB_ID} execution at: $(date)"
 
-# This file will be located in the directory mounted by the job.
-SESSION_FILE="delete_me_to_end_session"
-touch $SESSION_FILE
-
-# RUN JUPYTER SESSION IN BACKGROUND  -->  CAN STAY THE SAME
-LOCAL_IPY_PORT=8888
-
-# TAP Port
-LOCAL_PORT=5902
-
-NODE_HOSTNAME_PREFIX=$(hostname -s)   # Short Host Name  -->  name of compute node: c###-###
-NODE_HOSTNAME_DOMAIN=$(hostname -d)   # DNS Name  -->  stampede2.tacc.utexas.edu
-NODE_HOSTNAME_LONG=$(hostname -f)     # Fully Qualified Domain Name  -->  c###-###.stampede2.tacc.utexas.edu
+NODE_HOSTNAME_PREFIX=$(hostname -s)
+NODE_HOSTNAME_DOMAIN=$(hostname -d)
 
 echo "TACC: running on node $NODE_HOSTNAME_PREFIX on $NODE_HOSTNAME_DOMAIN"
 
@@ -31,25 +20,9 @@ else
     exit 1
 fi
 
-# Note: NotebookApp needs some work.
-JUPYTER_SERVER_APP="ServerApp"
-if [ ${JUPYTER_SERVER_APP} == "ServerApp" ]; then
-    JUPYTER_BIN="jupyter-lab"
-elif [ ${JUPYTER_SERVER_APP} == "NotebookApp" ]; then
-    JUPYTER_BIN="jupyter-notebook"
-fi
-echo "JUPYTER_SERVER_APP is $JUPYTER_SERVER_APP"
-echo "JUPYTER_BIN is $JUPYTER_BIN"
-
-NB_SERVERDIR=${HOME}/.jupyter
-
-# make .jupyter dir for logs
-mkdir -p ${NB_SERVERDIR}
-
-mkdir -p ${HOME}/.tap # this should exist at this point, but just in case...
-TAP_CERTFILE=${HOME}/.tap/.${SLURM_JOB_ID}
-
 # bail if we cannot create a secure session
+mkdir -p ${HOME}/.tap
+TAP_CERTFILE=$(cat ${HOME}/.tap/.${SLURM_JOB_ID})
 if [ ! -f ${TAP_CERTFILE} ]; then
     echo "TACC: ERROR - could not find TLS cert for secure session"
     echo "TACC: job ${SLURM_JOB_ID} execution finished at: $(date)"
@@ -64,6 +37,47 @@ if [ -z "${TAP_TOKEN}" ]; then
     exit 1
 fi
 echo "TACC: using token ${TAP_TOKEN}"
+
+# App Port
+LOCAL_PORT=8888
+
+# TAP Port
+LOGIN_PORT=$(tap_get_port)
+echo "TACC: got login node jupyter port ${LOGIN_PORT}"
+
+# Port forwarding is set up for up to four login nodes. Frontera has 4, LS6 has 3.
+#
+#   f: Requests ssh to go to background just before command execution.
+#      Used if ssh asks for passwords but the user wants it in the background. Implies -n too.
+#   g: Allows remote hosts to connect to local forwarded ports
+#   N: Do not execute a remote command. Useful for just forwarding ports.
+#   R: Connections to given TCP port/Unix socket on remote (server) host forwarded to local side.
+#
+# Create a reverse tunnel port from the compute node to the login nodes.
+# Make one tunnel for each login node so the user can just connect to $NODE_HOSTNAME_DOMAIN
+for i in `seq 4`; do
+  ssh -o StrictHostKeyChecking=no -f -g -N -R ${LOGIN_PORT}:${NODE_HOSTNAME_PREFIX}:${LOCAL_PORT} login${i}
+done
+
+JUPYTER_URL="https://${NODE_HOSTNAME_DOMAIN}:${LOGIN_PORT}/?token=${TAP_TOKEN}"
+echo "TACC:     JUPYTER_URL is $JUPYTER_URL"
+
+INTERACTIVE_WEBHOOK_URL="${_webhook_base_url}"
+
+# Wait a few seconds for jupyter to boot up and send webhook callback url for job ready notification.
+# Notification is sent to _INTERACTIVE_WEBHOOK_URL, e.g. https://3dem.org/webhooks/interactive/
+(
+    sleep 5 &&
+    curl -k --data "event_type=interactive_session_ready&address=${JUPYTER_URL}&owner=${_tapisJobOwner}&job_uuid=${_tapisJobUUID}" "${_INTERACTIVE_WEBHOOK_URL}" &
+) &
+
+# Note: NotebookApp needs some work.
+JUPYTER_SERVER_APP="ServerApp"
+if [ ${JUPYTER_SERVER_APP} == "ServerApp" ]; then
+    JUPYTER_BIN="jupyter-lab"
+elif [ ${JUPYTER_SERVER_APP} == "NotebookApp" ]; then
+    JUPYTER_BIN="jupyter-notebook"
+fi
 
 # create the tap jupyter config if needed
 TAP_JUPYTER_CONFIG="${HOME}/.tap/jupyter_config.py"
@@ -81,69 +95,10 @@ c.${JUPYTER_SERVER_APP}.ssl_options = {"ssl_version": ssl.PROTOCOL_TLSv1_2}
 c.IdentityProvider.token = "${TAP_TOKEN}"
 EOF
 
-# launch jupyter
-JUPYTER_LOGFILE=${NB_SERVERDIR}/${NODE_HOSTNAME_PREFIX}.log
+JUPYTER_ARGS="--certfile=${TAP_CERTFILE} --config=${TAP_JUPYTER_CONFIG}"
 
-JUPYTER_ARGS="--certfile=$(cat ${TAP_CERTFILE}) --config=${TAP_JUPYTER_CONFIG}"
 echo "TACC: using jupyter command: ${JUPYTER_BIN} ${JUPYTER_ARGS}"
-nohup ${JUPYTER_BIN} ${JUPYTER_ARGS} &> ${JUPYTER_LOGFILE} &
 
-JUPYTER_PID=$!
+${JUPYTER_BIN} ${JUPYTER_ARGS}
 
-LOGIN_PORT=$(tap_get_port)
-echo "TACC: got login node jupyter port ${LOGIN_PORT}"
-
-JUPYTER_URL="https://${NODE_HOSTNAME_DOMAIN}:${LOGIN_PORT}/?token=${TAP_TOKEN}"
-
-echo "JUPYTER_URL is $JUPYTER_URL"
-
-# verify jupyter is up. if not, give one more try, then bail
-if ! $(ps -fu ${USER} | grep ${JUPYTER_BIN} | grep -qv grep) ; then
-    # sometimes jupyter has a bad day. give it another chance to be awesome.
-    echo "TACC: first jupyter launch failed. Retrying..."
-    nohup ${JUPYTER_BIN} ${JUPYTER_ARGS} &> ${JUPYTER_LOGFILE} &
-fi
-if ! $(ps -fu ${USER} | grep ${JUPYTER_BIN} | grep -qv grep) ; then
-    # jupyter will not be working today. sadness.
-    echo "TACC: ERROR - jupyter failed to launch"
-    echo "TACC: ERROR - this is often due to an issue in your python or conda environment"
-    echo "TACC: ERROR - jupyter logfile contents:"
-    cat ${JUPYTER_LOGFILE}
-    echo "TACC: job ${SLURM_JOB_ID} execution finished at: $(date)"
-    exit 1
-fi
-
-# Port forwarding is set up for the four login nodes.
-#
-#   f: Requests ssh to go to background just before command execution.
-#      Used if ssh asks for passwords but the user wants it in the background. Implies -n too.
-#   g: Allows remote hosts to connect to local forwarded ports
-#   N: Do not execute a remote command. Useful for just forwarding ports.
-#   R: Connections to given TCP port/Unix socket on remote (server) host forwarded to local side.
-#
-# Create a reverse tunnel port from the compute node to the login nodes.
-# Make one tunnel for each login so the user can just connect to $NODE_HOSTNAME_DOMAIN
-for i in `seq 4`; do
-  ssh -o StrictHostKeyChecking=no -f -g -N -R ${LOGIN_PORT}:${NODE_HOSTNAME_PREFIX}:${LOCAL_PORT} login${i}
-done
-if [ $(ps -fu ${USER} | grep ssh | grep login | grep -vc grep) != 4 ]; then
-    # jupyter will not be working today. sadness.
-    echo "TACC: ERROR - ssh tunnels failed to launch"
-    echo "TACC: ERROR - this is often due to an issue with your ssh keys"
-    echo "TACC: ERROR - undo any recent mods in ${HOME}/.ssh"
-    echo "TACC: ERROR - or submit a TACC consulting ticket with this error"
-    echo "TACC: job ${SLURM_JOB_ID} execution finished at: $(date)"
-    exit 1
-fi
-
-# Webhook callback url for job ready notification.
-# Notification is sent to _INTERACTIVE_WEBHOOK_URL, e.g. https://3dem.org/webhooks/interactive/
-curl -k --data "event_type=interactive_session_ready&address=${JUPYTER_URL}&owner=${_tapisJobOwner}&job_uuid=${_tapisJobUUID}" "${_INTERACTIVE_WEBHOOK_URL}" &
-
-# Delete the session file to kill the job.
-echo $NODE_HOSTNAME_LONG $IPYTHON_PID > $SESSION_FILE
-
-# While the session file remains undeleted, keep Jupyter session running.
-while [ -f $SESSION_FILE ] ; do
-    sleep 10
-done
+echo "TACC: release port returned $(tap_release_port ${LOGIN_PORT} 2> /dev/null)"
