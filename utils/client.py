d3be4f1ed1319035044d10cad11d324647a94449
Chandra Y <cyemparala@tacc.utexas.edu>
2024-02-08 21:21:45 -0600

    Convert Align Swift to ZIP Runtime (#16)
    Co-authored-by: Sal Tijerina <r.sal.tijerina@gmail.com>

--- utils/client.py
+++ utils/client.py
@@ -1,5 +1,5 @@
 from tapipy.tapis import Tapis
 
 
-def get_client(**credentials):
-    return Tapis(**credentials, download_latest_specs=True)
+def get_client(use_dev_specs, **credentials):
+    return Tapis(**credentials, download_latest_specs=True, resource_set='dev') if use_dev_specs else Tapis(**credentials, download_latest_specs=True)
