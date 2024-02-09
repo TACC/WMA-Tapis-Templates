8aeb2a824b332364f4d587244e5aa4ce83191016
Chandra Y <cyemparala@tacc.utexas.edu>
2024-02-08 21:21:45 -0600

    Convert Align Swift to ZIP Runtime (#16)
    Co-authored-by: Sal Tijerina <r.sal.tijerina@gmail.com>

--- initialize_tenant.py
+++ initialize_tenant.py
@@ -94,6 +94,8 @@ def main():
                         help='Comma separated list of systems. Paramater only applies for single tenant case')
     parser.add_argument('-a', '--apps',
                         help='Comma separated list of apps. Paramater only applies for single tenant case')
+    parser.add_argument('-d', '--dev', action='store_false', default=False,
+                        help='Uses Dev Specs when enabled')
     args = parser.parse_args()
 
     if args.tenants:
@@ -155,7 +157,7 @@ def main():
 
         for credentials in TAPIS_CLIENTS.get(tenant_name, []):
             print(f"provisioning tenant: {credentials['base_url']}")
-            client = get_client(**credentials)
+            client = get_client(args.dev, **credentials)
             provision(client, systems, apps, args)
 
 
