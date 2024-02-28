91aa1153df7d24489d0372212d87a1d186c4c48d
Van Go <35277477+van-go@users.noreply.github.com>
2024-02-28 12:51:22 -0600

    Update app.json and run.sh for OpenSees v3

--- applications/opensees-v3/run.sh
+++ applications/opensees-v3/run.sh
@@ -1,20 +1,25 @@
 set -x
-WRAPPERDIR=$( cd "$( dirname "$0" )" && pwd )
 
-echo "inputScript is ${inputScript}"
 INPUTSCRIPT='${inputScript}'
+
 echo "INPUTSCRIPT is $INPUTSCRIPT"
+
 TCLSCRIPT="${INPUTSCRIPT##*/}"
 
 echo "TCLSCRIPT is $TCLSCRIPT"
 
-docker run -i --user 458981:816877 --sig-proxy=true --rm \
-			-m 1G --name "opensees_${AGAVE_JOB_OWNER}_${AGAVE_JOB_ID}"\
-			-v "`pwd`/${inputDirectory}":"/data/" \
-			wangyinz/opensees-container /bin/sh -c "cd /data ; OpenSees < /data/$TCLSCRIPT"
+cd "${inputDirectory}"
+
+echo "Running OpenSeesSP"
+
+ibrun OpenSeesSP $TCLSCRIPT
 
 if [ ! $? ]; then
-	echo "Docker container exited with an error status. $?" >&2
-	${AGAVE_JOB_CALLBACK_FAILURE}
-	exit
+
+    echo "OpenSees exited with an error status. $?" >&2
+
+    exit 1
+
 fi
+
+cd ..
