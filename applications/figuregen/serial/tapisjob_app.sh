286c9d477be54cf16a61e5478cde55792f3816ba
Chandra Y <cyemparala@tacc.utexas.edu>
2024-03-01 12:07:26 -0600

    Figure - parallel and serial
    FigureGen parallel and serial in frontera

---
+++ applications/figuregen/serial/tapisjob_app.sh
@@ -0,0 +1,19 @@
+set -x
+WRAPPERDIR=$( cd "$( dirname "$0" )" && pwd )
+
+ #Change to input directory
+cd inputDirectory
+inputfile=$1
+
+# Do not set GMT and GhostScript paths, they are available via the container
+sed -e "4s/.*/                                                   ! Path where GMT executables are located./" -i ${inputfile}
+sed -e "5s/.*/                                                   ! Path where GhostScript executable is located./" -i ${inputfile}
+
+
+apptainer exec library://georgiastuart/figuregen/figuregen-serial figuregen -I ${inputfile} -O fig.out
+cd ..
+
+if [ ! $? ]; then
+        echo "figuregen exited with an error status. $?" >&2
+        exit 1
+fi
