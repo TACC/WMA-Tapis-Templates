687dc1fc0a609d2bda4b9f11921045bab2b48373
Taylor Grafft <tgrafft@TACCs-MacBook-Pro.local>
2024-03-06 13:53:13 -0600

    task/WA-149: Kalpana v3 conversion

---
+++ applications/kalpana/tapisjob_app.sh
@@ -0,0 +1,28 @@
+set -xe
+#loading required modules
+module load python3
+
+#installing lib
+pip3 install --user shapely
+pip3 install --user fiona
+pip3 install --user simplekml
+pip3 install --user netCDF4
+echo "Sucessfully installed"
+
+#path where the python script lives
+git clone https://github.com/TACC/Kalpana.git
+
+sleep 5
+
+pwd
+mkdir output
+mv ${filetype} output/
+
+cd output/
+
+#execute python script
+python3 ../Kalpana/Kalpana_N.py --storm test --filetype ${filetype} --polytype ${polytype} --viztype shapefile --subplots no --${contour} "${range}"
+
+sleep 10
+
+rm -rf ../Kalpana
