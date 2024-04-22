e8483c61324004b611ac00706f3d5447ab30ec68
Asim Regmi <asimregmi.ar@gmail.com>
2024-04-22 10:45:48 -0500

    process multiple case files

--- applications/LS-Dyna/tapisjob_app.sh
+++ applications/LS-Dyna/tapisjob_app.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
 set -x
 WRAPPERDIR=$( cd "$( dirname "$0" )" && pwd )
 cd inputDirectory
 
 # Parameters
-
 inputFile=$1
 precision=$pre
 nodes=$_tapisNodes
@@ -15,8 +13,8 @@ v2="D"
 
 export LSTC_LICENSE=network
 export LSTC_LICENSE_SERVER=31010@license03.tacc.utexas.edu
-# Choose the appropriate binary based on the precision
 
+# Choose the appropriate binary based on the precision
 if [ "$precision" == "$v1" ]; then
     lsdyna_bin="/home1/apps/ANSYS/2023R2/v232/ansys/bin/linx64/lsdyna_sp_mpp.e"
 elif [ "$precision" == "$v2" ]; then
@@ -29,22 +27,29 @@ fi
 # Calculate total number of processors
 total_procs=$((nodes*ppn))
 
-echo "Running LS-DYNA for input file: $k_file with $total_procs total processors using binary $lsdyna_bin."
+while IFS= read -u 9 k_file; do
+    if [[ -f "$k_file" ]]; then
+        k_file_path=$(realpath "$k_file")
+        sim_dir="simulation_${k_file%.k}"
+        mkdir -p "${sim_dir}"
+        cd "${sim_dir}"
 
-if [[ -f "$inputFile" ]]; then
-    echo "Running LS-DYNA for input file: $inputFile with $total_procs total processors using binary $lsdyna_bin."
-    ibrun -N=$nodes -n=$total_procs ${lsdyna_bin} i="${inputFile}" > "log_${inputFile%.k}.txt" 2>&1
+        echo "Running LS-DYNA for input file: $k_file with $total_procs total processors using binary $lsdyna_bin."
+        ibrun ${lsdyna_bin} i=${k_file_path} > "log_${k_file%.k}.txt" 2>&1
 
-    # Check if LS-DYNA finished successfully
-    if [ $? -ne 0 ]; then
-        echo "LS-DYNA simulation for input file: $inputFile exited with an error status. $?" >&2
-        exit 1
+        if [ $? -ne 0 ]; then
+            echo "LS-DYNA simulation for input file: $k_file exited with an error status $status." >&2
+            exit 1
+        else
+            echo "LS-DYNA simulation for input file: $k_file completed successfully."
+        fi
+
+	cd -
     else
-        echo "LS-DYNA simulation for input file: $inputFile completed successfully."
+        echo "Input file $k_file does not exist."
     fi
-else
-    echo "Input file $inputFile does not exist."
-fi
+
+done 9< "$inputFile"
 
 # Finish the job
 echo "Job ${_SLURM_JOB_ID} execution finished at: $(date)"
