2bfdaa9a03bef4f5fea4fcd1fdad8f3dc7127ae8
Chandra Y <cyemparala@tacc.utexas.edu>
2024-04-22 11:32:31 -0500

    QGIS Express App

---
+++ applications/qgis-express/README.md
@@ -0,0 +1,10 @@
+### QGIS Binary build instructions:
+
+1. On wma-dcv-01 system, sudo as root.
+2. Follow the instructions for ubuntu image version: https://github.com/qgis/QGIS/blob/final-3_36_2/INSTALL.md
+3. Note the version is 3.36.2
+4. setup cmake to use /opt/qgis/3.36 folder as install folder.
+5. Use startup.py for qgis, this is similar to agave version in designsafe.
+
+### Sample data:
+ There is sample data in /corral-repl/tacc/aci/CEP/community/app_examples/qgis/
---
+++ applications/qgis-express/tapisjob_app.sh
@@ -0,0 +1,65 @@
+set -x
+echo "TACC: job $_tapisJobUUID execution at: `date`"
+
+# confirm DCV server is alive
+DCV_SERVER_UP=`systemctl is-active dcvserver`
+if [ $DCV_SERVER_UP != "active" ]; then
+  echo "TACC:"
+  echo "TACC: ERROR - could not confirm dcvserver active, systemctl returned '$DCV_SERVER_UP'"
+  echo "TACC: ERROR - Please submit a consulting ticket at the TACC user portal"
+  echo "TACC: ERROR - https://portal.tacc.utexas.edu/tacc-consulting/-/consult/tickets/create"
+  echo "TACC:"
+  echo "TACC: job $_tapisJobUUID execution finished at: `date`"
+  exit 1
+fi
+
+# Run this script on session startup
+DCV_STARTUP="/tmp/dcv-startup-${_tapisJobUUID}"
+DCV_HANDLE="${_tapisJobUUID}-session"
+cat <<- EOF > $DCV_STARTUP
+#!/bin/sh
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/qgis/3.36/lib
+/opt/qgis/3.36/bin/qgis --noversioncheck --profiles-path ~/.qgis --code /opt/qgis/3.36/startup.py
+
+dcv close-session ${DCV_HANDLE}
+EOF
+chmod a+rx $DCV_STARTUP
+
+# create DCV session for this job
+dcv create-session --owner ${_tapisJobOwner} --init ${DCV_STARTUP} $DCV_HANDLE
+if ! `dcv list-sessions | grep -q ${_tapisJobUUID}`; then
+  echo "TACC:"
+  echo "TACC: WARNING - could not find a DCV session for this job"
+  echo "TACC: WARNING - This could be because all DCV licenses are in use."
+  echo "TACC: WARNING - Failing over to VNC session."
+  echo "TACC: "
+  echo "TACC: If you rarely receive a DCV session using this script, "
+  echo "TACC: please submit a consulting ticket at the TACC user portal:"
+  echo "TACC: https://portal.tacc.utexas.edu/tacc-consulting/-/consult/tickets/create"
+  echo "TACC: "
+  echo "TACC: job $_tapisJobUUID execution finished at: `date`"
+  exit 1
+fi
+
+LOCAL_PORT="8443"  # default DCV port
+
+# Webhook callback url for job ready notification
+# (notifications sent to INTERACTIVE_WEBHOOK_URL (i.e. https://3dem.org/webhooks/interactive/))`
+INTERACTIVE_WEBHOOK_URL="${_INTERACTIVE_WEBHOOK_URL}"
+
+#connect to DCV session on VM
+curl -k --data "event_type=interactive_session_ready&address=https://wma-dcv-01.tacc.utexas.edu:${LOCAL_PORT}/#${DCV_HANDLE}&owner=${_tapisJobOwner}&job_uuid=${_tapisJobUUID}" $INTERACTIVE_WEBHOOK_URL &
+
+echo "TACC: Your DCV session is now running!"
+echo "TACC: Connect to your session at: https://wma-dcv-01.tacc.utexas.edu:${LOCAL_PORT}/#${DCV_HANDLE}"
+
+# Keep script running while dcv session is active
+while dcv list-sessions | grep -q ${DCV_HANDLE}; do
+  sleep 5;
+done
+echo "TACC: DCV session has been closed."
+
+# remove X11 sockets
+find /tmp/.X11-unix -user $USER -exec rm -f '{}' \;
+
+echo "TACC: job $_tapisJobUUID execution finished at: `date`"
