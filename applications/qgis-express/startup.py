9d463da5cd9b6ec71024359fab233c64f7be5c70
Chandra Y <cyemparala@tacc.utexas.edu>
2024-08-09 16:39:47 -0500

    NHERI folder shortcuts for Matlab express and QGIS express

--- applications/qgis-express/startup.py
+++ applications/qgis-express/startup.py
@@ -1,5 +1,4 @@
 import os
-
 from qgis.utils import iface
 from qgis.core import QgsExpressionContextUtils, QgsMessageLog
 
@@ -13,35 +12,30 @@ community = os.path.join(os.environ['HOME'], 'CommunityData')
 
 base_directory = os.path.dirname(new_favorite_directory)
 
-
 def setup_favorites():
     browser_model = iface.browserModel()
 
-    # Try to remove any previously added directories
-    try:
-        favorite_index = browser_model.findPath("favorites:")
-        to_be_removed = []
-        for i in range(browser_model.rowCount(favorite_index)):
-            index = browser_model.index(i, 0, favorite_index)
-            path = index.data()
-            dirname = os.path.dirname(path)
-            if base_directory == dirname or path in ['MyData', 'NEES', 'NHERI-Published','CommunityData']:
-                to_be_removed.append(index)
-        for remove_index in to_be_removed:
-            browser_model.removeFavorite(remove_index)
-    except Exception as e:
-        QgsMessageLog.logMessage(f"Error removing old favorites: {str(e)}", level=QgsMessageLog.CRITICAL)
+    # Fetch existing favorites
+    favorite_index = browser_model.findPath("favorites:")
+    existing_favorites = set()
 
-    # Add the new directories as favorites
+    for i in range(browser_model.rowCount(favorite_index)):
+        index = browser_model.index(i, 0, favorite_index)
+        path = os.path.normpath(index.data())
+        existing_favorites.add(path)
+
+    # Add the new directories as favorites if not already present
     try:
-        
-        browser_model.addFavoriteDirectory(mydata)
-        browser_model.addFavoriteDirectory(nees)
-        browser_model.addFavoriteDirectory(nheriPublished)
-        browser_model.addFavoriteDirectory(community)
+        if os.path.normpath(mydata) not in existing_favorites:
+            browser_model.addFavoriteDirectory(mydata)
+        if os.path.normpath(nees) not in existing_favorites:
+            browser_model.addFavoriteDirectory(nees)
+        if os.path.normpath(nheriPublished) not in existing_favorites:
+            browser_model.addFavoriteDirectory(nheriPublished)
+        if os.path.normpath(community) not in existing_favorites:
+            browser_model.addFavoriteDirectory(community)
     except Exception as e:
         QgsMessageLog.logMessage(f"Error adding new favorites: {str(e)}", level=QgsMessageLog.CRITICAL)
-
 
 def customize_title():
     try:
