20593006d22000c05e309baa54fe73cf2fbecb6f
Garrett Edmonds <edmondsgarrett@gmail.com>
2024-08-30 16:32:59 -0500

    SimCenter DesignSafe VM v0.0.1

---
+++ applications/simcenter-designsafe-vm/tapisjob_app.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+handle_error() {
+  local EXITCODE=$1
+  echo "Phyiscs-based extractor job exited with an error status. $EXITCODE" >&2
+  echo "$EXITCODE" > "${_tapisExecSystemOutputDir}/tapisjob.exitcode"
+  exit $EXITCODE
+}
+
+set -x
+
+PBESCRIPT="${inputScript##*/}"
+echo "PBESCRIPT is $PBESCRIPT"
+
+inputPath="`pwd`/${inputDirectory}"
+INPUTDIR=${inputPath}
+if [ -z ${inputDirectory} ]; then
+    INPUTDIR="/corral/projects/NHERI/shared/${_tapisJobOwner}"
+fi
+
+#dataPath="`pwd`/${dataDirectory}"
+DATADIR_NAME="${dataDirectory##*/}"
+DATADIR=`python3 get_folder_mount_path.py $dataDirectory`
+
+DATADIR_MOUNT="--bind ${DATADIR}:/home/jovyan/work/projects/$DATADIR_NAME"
+if [ -z ${dataDirectory} ]; then
+    projects_dir="$HOME/MyProjects"
+
+    mkdir -p "$projects_dir"
+    IFS=' ' read -r -a projects <<< "${_UserProjects}"
+    for project in "${projects[@]}"; do
+        IFS=',' read -r uuid projectId <<< "$project"
+        target_path="/corral/main/projects/NHERI/projects/$uuid"
+        symlink_path="$projects_dir/$projectId"
+
+        if [ -e "$target_path" ]; then
+            if [ ! -e "$symlink_path" ]; then
+                ln -s "$target_path" "$symlink_path"
+                echo "TACC: Project Links: Created symlink: $symlink_path -> $target_path"
+            else
+                echo "TACC: Project Links: Symlink already exists: $symlink_path"
+            fi
+        else
+            echo "TACC: Project Links: Target path does not exist: $target_path"
+        fi
+    done
+    DATADIR_MOUNT="--bind ${projects_dir}:/home/jovyan/work/projects"
+fi
+
+echo "Running $PBESCRIPT"
+
+apptainer run \
+    --writable-tmpfs \
+    --memory 10G \
+    --bind $INPUTDIR:"/data/" \
+    --bind "/corral/main/projects/NHERI/shared/${_tapisJobOwner}":"/home/jovyan/work/MyData" \
+    --bind /corral/main/projects/NHERI/public/projects:/home/jovyan/work/NEES:ro \
+    --bind /corral/main/projects/NHERI/community:/home/jovyan/work/CommunityData:ro \
+    --bind /corral/main/projects/NHERI/published:/home/jovyan/work/NHERI-Published:ro \
+    --bind /corral/main/projects/NHERI/projects \
+    $DATADIR_MOUNT \
+    docker://taccaci/designsafe-simcenter-vm:0.0.1 /bin/sh -c "cd /data; python3 /data/$PBESCRIPT"
+
+if [ ! $? ]; then
+    handle_error 1
+    exit
+fi
