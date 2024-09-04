e260fc7a325b75308f6b72a337a1e9d4f6b1d7b0
Sal Tijerina <r.sal.tijerina@gmail.com>
2024-09-04 16:47:22 -0500

    update init script

--- initialize_tenant.py
+++ initialize_tenant.py
@@ -1,3 +1,23 @@
+"""
+This script is used to provision or update a tenant with systems, apps, and scheduler profiles.
+Usage:
+    python initialize_tenant.py [-t TENANTS] [-up] [-s SYSTEMS] [-us] [-a APPS] [-ua] [-d]
+Options:
+    -t, --tenants TENANTS: Comma separated list of tenants.
+    -up, --update-profiles: Force recreate scheduler profiles.
+    -s, --systems SYSTEMS: Comma separated list of systems.
+    -us, --update-systems: Update systems if they already exist.
+    -a, --apps APPS: Comma separated list of apps.
+    -ua, --update-apps: Update apps if they already exist.
+    -d, --dev: Uses Dev Specs when enabled.
+    Provisions or updates a tenant with systems, apps, and scheduler profiles.
+        client (Client): The client object used to interact with the systems, apps, and profiles.
+        systems (list): A list of systems to provision or update.
+        apps (list): A list of apps to provision or update.
+        args (argparse.Namespace): The command line arguments.
+    The main function that is executed when the script is run.
+"""
+
 import os
 import argparse
 from tapipy.errors import BaseTapyException
@@ -7,6 +27,18 @@ from utils.client import get_client
 
 
 def get_or_create_system(client, system_def, update=False):
+    """
+    Retrieves an existing system or creates a new system based on the provided system definition.
+    Args:
+        client (Client): The client object used to interact with the system.
+        system_def (dict): The system definition containing the necessary information to create or update the system.
+        update (bool, optional): Specifies whether to update the system if it already exists. Defaults to False.
+    Raises:
+        BaseTapyException: If an error occurs while interacting with the system.
+    Returns:
+        None
+    """
+
     system_id = system_def["id"]
     try:
         client.systems.getSystem(systemId=system_id)
@@ -23,6 +55,21 @@ def get_or_create_system(client, system_def, update=False):
 
 
 def provision(client, systems, apps, args):
+    """
+    Provision the client, systems, and apps based on the given arguments.
+
+    Args:
+        client: The client object used for API calls.
+        systems: A list of system names.
+        apps: A list of application names.
+        args: Additional arguments.
+
+    Raises:
+        BaseTapyException: If an error occurs during API calls.
+
+    Returns:
+        None
+    """
     profile = load_file_to_json("profiles/tacc-apptainer.json")
     try:
         client.systems.createSchedulerProfile(**profile)
@@ -87,6 +134,21 @@ def provision(client, systems, apps, args):
 
 
 def main():
+    """
+    Initialize Tenant
+    Provisions or updates a tenant with systems, apps, and scheduler profiles.
+    Arguments:
+    -t, --tenants: Comma separated list of tenants
+    -up, --update-profiles: Force recreate scheduler profiles
+    -s, --systems: Comma separated list of systems
+    -us, --update-systems: Update systems if they already exist
+    -a, --apps: Comma separated list of apps
+    -ua, --update-apps: Update apps if they already exist
+    -d, --dev: Uses Dev Specs when enabled
+    Returns:
+    None
+    """
+
     parser = argparse.ArgumentParser(
         prog="Initialize Tenant",
         description="Provision or update a tenant with systems, apps, and scheduler profiles",
@@ -143,6 +205,7 @@ def main():
         "stampede3",
         "wma-dcv-01",
         "wma-exec-01",
+        "vista",
     ]
 
     for tenant_name in tenant_names:
@@ -192,8 +255,8 @@ def main():
                         "figuregen/serial",
                         "figuregen/parallel",
                         "GiD-stampede3",
-                        "jupyter-lab-hpc",
-                        "jupyter-lab-hpc/gpu",
+                        "jupyter-lab-hpc-s3",
+                        "jupyter-lab-hpc-cuda-ds",
                         "kalpana",
                         "LS-Dyna-stampede3",
                         "ls-pre-post-stampede3",
@@ -222,17 +285,13 @@ def main():
             case "APCD":
                 systems = (
                     all_systems
-                    [
+                    + [
                         "apcd/apcd.submissions",
                     ]
                     if systems == ["ALL"]
                     else systems
                 )
-                apps = (
-                    []
-                    if apps == ["ALL"]
-                    else apps
-                )
+                apps = [] if apps == ["ALL"] else apps
             case _:
                 systems = all_systems if systems == ["ALL"] else systems
                 apps = (
