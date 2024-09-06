c954f715c1da9fa43050769edcc458bedcd48a84
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2024-09-06 16:11:02 -0500

    update app.json; remove env file from ZIP archive

--- applications/setfacl-corral/getUID.sh
+++ applications/setfacl-corral/getUID.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source tas.env
+source /data/designsafe/mydata/wma_prtl/applications/v3/setfacl-corral-tg458981/v002/tas.env
 USER=$1
 curl -sku $tas_user:$tas_pw https://tas.tacc.utexas.edu/api/v1/users/username/$USER | ./JSON.sh | awk '/^\["result","uid"/{print $NF}'
