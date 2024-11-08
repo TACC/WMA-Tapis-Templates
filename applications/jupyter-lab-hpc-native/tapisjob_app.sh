e3eb6d009991d7f6b00b9e96233594a4c5a40714
Van Go <35277477+van-go@users.noreply.github.com>
2024-11-08 15:38:47 -0600

    Update Jupyter Lab HPC Native version to 1.0.0

--- applications/jupyter-lab-hpc-native/tapisjob_app.sh
+++ applications/jupyter-lab-hpc-native/tapisjob_app.sh
@@ -5,152 +5,88 @@ echo "TACC: job ${SLURM_JOB_ID} execution at: $(date)"
 # TAP Port
 LOCAL_PORT=5902
 
-mkdir -p $PWD/Jupyter
-cd Jupyter
-
-# Launch jupyter-lab and hang the job on this process directly
-jupyter-lab ${JUPYTER_ARGS}
-
-# Make symlinks for work, home, scratch, and mydata
-mkdir -p $PWD/Work $PWD/Home $PWD/Scratch $PWD/MyData
-
-if [ ! -L $PWD/Work ]; then
-    ln -s $STOCKYARD $PWD/Work
-fi
-
-if [ ! -L $PWD/Home ]; then
-    ln -s $HOME $PWD/Home
-fi
-
-if [ ! -L $PWD/Scratch ]; then
-    ln -s $SCRATCH $PWD/Scratch
-fi
-
-if [ ! -L $PWD/MyData ]; then
-    ln -s /data/designsafe/mydata/${_tapisJobOwner} $PWD/MyData
-fi
-
+# Ensure TAP functions are available
 TAP_FUNCTIONS="/share/doc/slurm/tap_functions"
 if [ -f ${TAP_FUNCTIONS} ]; then
     . ${TAP_FUNCTIONS}
 else
-    echo "TACC:"
     echo "TACC: ERROR - could not find TAP functions file: ${TAP_FUNCTIONS}"
-    echo "TACC: ERROR - Please submit a consulting ticket at the TACC user portal"
-    echo "TACC: ERROR - https://portal.tacc.utexas.edu/tacc-consulting/-/consult/tickets/create"
-    echo "TACC: job $SLURM_JOB_ID execution finished at: `date`"
     exit 1
 fi
 
-# our node name
-NODE_HOSTNAME_PREFIX=$(hostname -s)
+# Set LOGIN_PORT using tap_get_port and verify it succeeds
+LOGIN_PORT=$(tap_get_port)
+if [ -z "${LOGIN_PORT}" ]; then
+    echo "TACC: ERROR - could not retrieve login port."
+    exit 1
+fi
+
+# Retrieve node domain for Jupyter URL
 NODE_HOSTNAME_DOMAIN=$(hostname -d)
-NODE_HOSTNAME_LONG=$(hostname -f)
 
-echo "TACC: running on node $NODE_HOSTNAME_PREFIX on $NODE_HOSTNAME_DOMAIN"
-
-echo "TACC: unloading xalt"
-module unload xalt
-
-# Use jupyter-lab, exit if it's not found
-JUPYTER_BIN=$(which jupyter-lab 2> /dev/null)
-if [ -z "${JUPYTER_BIN}" ]; then
-    echo "TACC: ERROR - could not find jupyter-lab install"
-    echo "TACC: loaded modules below"
-    module list
-    echo "TACC: job ${SLURM_JOB_ID} execution finished at: $(date)"
-    exit 1
-else
-    JUPYTER_SERVER_APP="ServerApp"
-fi
-echo "TACC: using jupyter binary ${JUPYTER_BIN}"
-
-NB_SERVERDIR=${HOME}/.jupyter
-IP_CONFIG=${NB_SERVERDIR}/jupyter_notebook_config.py
-
-# make .jupyter dir for logs
-mkdir -p ${NB_SERVERDIR}
-mkdir -p ${HOME}/.tap
-
-TAP_LOCKFILE=${HOME}/.tap/.${SLURM_JOB_ID}.lock
-TAP_CERTFILE=${HOME}/.tap/.${SLURM_JOB_ID}
-
-# bail if we cannot create a secure session
-if [ ! -f ${TAP_CERTFILE} ]; then
-    echo "TACC: ERROR - could not find TLS cert for secure session"
-    echo "TACC: job ${SLURM_JOB_ID} execution finished at: $(date)"
-    exit 1
-fi
-
-# bail if we cannot create a token for the session
+# Generate or retrieve TAP_TOKEN
+TAP_CERTFILE="${HOME}/.tap/.${SLURM_JOB_ID}"
 TAP_TOKEN=$(tap_get_token)
 if [ -z "${TAP_TOKEN}" ]; then
     echo "TACC: ERROR - could not generate token for notebook"
-    echo "TACC: job ${SLURM_JOB_ID} execution finished at: $(date)"
     exit 1
 fi
 echo "TACC: using token ${TAP_TOKEN}"
 
-# create the tap jupyter config if needed
+# Define Jupyter Arguments
+JUPYTER_ARGS="--port=${LOCAL_PORT} --certfile=$(cat ${TAP_CERTFILE}) --config=${TAP_JUPYTER_CONFIG} --ServerApp.token=${TAP_TOKEN}"
+
+# Set up Jupyter configuration
 TAP_JUPYTER_CONFIG="${HOME}/.tap/jupyter_config.py"
+mkdir -p $(dirname ${TAP_JUPYTER_CONFIG})
 cat <<- EOF > ${TAP_JUPYTER_CONFIG}
-# Configuration file for TAP jupyter-lab
 import ssl
 c = get_config()
 c.IPKernelApp.pylab = "inline"
 c.ServerApp.ip = "0.0.0.0"
-c.ServerApp.port = 5902
+c.ServerApp.port = ${LOCAL_PORT}
 c.ServerApp.open_browser = False
 c.ServerApp.allow_origin = u"*"
 c.ServerApp.ssl_options={"ssl_version": ssl.PROTOCOL_TLSv1_2}
 c.ServerApp.mathjax_url = u"https://cdn.mathjax.org/mathjax/latest/MathJax.js"
 EOF
 
-# Set Jupyter Lab settings directory
-export JUPYTERLAB_SETTINGS_DIR="${HOME}/.jupyterlab"
+echo "Jupyter configuration file contents:"
+cat ${TAP_JUPYTER_CONFIG}
 
-# launch jupyter-lab
-JUPYTER_LOGFILE=${NB_SERVERDIR}/${NODE_HOSTNAME_PREFIX}.log
-JUPYTER_ARGS="--certfile=$(cat ${TAP_CERTFILE}) --config=${TAP_JUPYTER_CONFIG} --${JUPYTER_SERVER_APP}.token=${TAP_TOKEN}"
-echo "TACC: using jupyter command: ${JUPYTER_BIN} ${JUPYTER_ARGS}"
-nohup ${JUPYTER_BIN} ${JUPYTER_ARGS} &> ${JUPYTER_LOGFILE} && rm ${TAP_LOCKFILE} &
+# Use Jupyter binary, exit if not found
+JUPYTER_BIN=$(which jupyter-lab 2> /dev/null)
+if [ -z "${JUPYTER_BIN}" ]; then
+    echo "TACC: ERROR - could not find jupyter-lab install"
+    exit 1
+fi
+echo "TACC: using jupyter binary ${JUPYTER_BIN}"
+
+# Launch JupyterLab with nohup
+nohup ${JUPYTER_BIN} ${JUPYTER_ARGS} &> "${HOME}/.jupyter/jupyter_${SLURM_JOB_ID}.log" &
 JUPYTER_PID=$!
 
-LOGIN_PORT=$(tap_get_port)
-echo "TACC: got login node jupyter port ${LOGIN_PORT}"
-
-JUPYTER_URL="https://${NODE_HOSTNAME_DOMAIN}:${LOGIN_PORT}/?token=${TAP_TOKEN}"
-
-# verify jupyter is up. if not, give one more try, then bail
-if ! $(ps -fu ${USER} | grep ${JUPYTER_BIN} | grep -qv grep) ; then
-    echo "TACC: first jupyter launch failed. Retrying..."
-    nohup ${JUPYTER_BIN} ${JUPYTER_ARGS} &> ${JUPYTER_LOGFILE} && rm ${TAP_LOCKFILE} &
-fi
-if ! $(ps -fu ${USER} | grep ${JUPYTER_BIN} | grep -qv grep) ; then
-    echo "TACC: ERROR - jupyter failed to launch"
-    echo "TACC: ERROR - this is often due to an issue in your python or conda environment"
-    echo "TACC: ERROR - jupyter logfile contents:"
-    cat ${JUPYTER_LOGFILE}
-    echo "TACC: job ${SLURM_JOB_ID} execution finished at: $(date)"
+# Validate JupyterLab launch
+if ! ps -p ${JUPYTER_PID} > /dev/null; then
+    echo "TACC: ERROR - Jupyter failed to launch."
     exit 1
 fi
 
-# create reverse tunnel port to login nodes.
+# Create reverse SSH tunnel for each login node
 NUM_LOGINS=4
 for i in $(seq ${NUM_LOGINS}); do
     ssh -o StrictHostKeyChecking=no -q -f -g -N -R ${LOGIN_PORT}:${NODE_HOSTNAME_PREFIX}:${LOCAL_PORT} login${i}
 done
+
 if [ $(ps -fu ${USER} | grep ssh | grep login | grep -vc grep) != ${NUM_LOGINS} ]; then
-    echo "TACC: ERROR - ssh tunnels failed to launch"
+    echo "TACC: ERROR - SSH tunnels failed to establish."
     exit 1
 fi
-echo "TACC: created reverse ports on Frontera logins"
+echo "TACC: created reverse ports on system logins"
 
-echo "Sourcing tapisjob.env..."
-source "${_tapisJobWorkingDir}/tapisjob.env"
-
-echo "TACC: Your jupyter-lab server is now running at ${JUPYTER_URL}"
+# Final URL and webhook notification
+JUPYTER_URL="https://${NODE_HOSTNAME_DOMAIN}:${LOGIN_PORT}/?token=${TAP_TOKEN}"
 curl -k --data "event_type=interactive_session_ready&address=${JUPYTER_URL}&owner=${_tapisJobOwner}&job_uuid=${_tapisJobUUID}" "${_INTERACTIVE_WEBHOOK_URL}"
 
-echo "TACC: release port returned $(tap_release_port ${LOGIN_PORT})"
-sleep 1
+echo "TACC: Your JupyterLab server is now running at ${JUPYTER_URL}"
+echo "TACC: job ${SLURM_JOB_ID} execution finished at: $(date)"
