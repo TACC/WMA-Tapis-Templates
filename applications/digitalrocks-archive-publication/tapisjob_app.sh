6037006c7c9d748c5e2570b5d490b40114b1a201
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2025-01-27 16:48:55 -0600

    Update script to fix paths within archive

--- applications/digitalrocks-archive-publication/tapisjob_app.sh
+++ applications/digitalrocks-archive-publication/tapisjob_app.sh
@@ -2,6 +2,12 @@
 
 set -x
 
-zip -r ${publishedRootDir}/archive/${projectId}_archive.zip \
-       ${publishedRootDir}/archive/${projectId}_metadata.json \
-       ${publishedRootDir}/${projectId}
+pushd ${publishedRootDir}
+zip -r archive/${projectId}_archive.zip ${projectId}
+
+# Move to archive folder to add metadata JSON at the top level of the archive
+pushd archive
+zip -u ${projectId}_archive.zip ${projectId}_metadata.json \
+
+popd
+popd
