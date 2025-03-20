9926e77a0840fb7f15a56550ead01bc94b38f3ce
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2025-03-20 13:39:57 -0500

    update DRP archive app to use subfolder

--- applications/digitalrocks-archive-publication/tapisjob_app.sh
+++ applications/digitalrocks-archive-publication/tapisjob_app.sh
@@ -3,10 +3,11 @@
 set -x
 
 pushd ${publishedRootDir}
-zip -r archive/${projectId}_archive.zip ${projectId}
+mkdir -p archive/${projectId}
+zip -r archive/${projectId}/${projectId}_archive.zip ${projectId}
 
 # Move to archive folder to add metadata JSON at the top level of the archive
-pushd archive
+pushd archive/${projectId}
 zip -u ${projectId}_archive.zip ${projectId}_metadata.json \
 
 popd
