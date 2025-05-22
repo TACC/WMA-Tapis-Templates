a26535a09c47cf90cde5a28b98b317be265df9d5
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2025-05-22 13:47:06 -0500

    script fix

--- applications/digitalrocks-archive-publication/tapisjob_app.sh
+++ applications/digitalrocks-archive-publication/tapisjob_app.sh
@@ -12,8 +12,8 @@ zip -r archive/${projectId}/${projectId}_archive.zip ${projectId}
 
 # Move to archive folder to add metadata JSON at the top level of the archive
 pushd archive/${projectId}
-zip -u ${projectId}_archive.zip ${projectId}_metadata.json \
-chmod 755 ${projectId}_archive.zip
+zip -u ${projectId}_archive.zip ${projectId}_metadata.json
+chmod -R 755 ${projectId}_archive.zip
 
 popd
 popd
