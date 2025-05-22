d57365177632bfc2f82ad04fcbe0ee92c2ae3040
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2025-05-22 13:45:21 -0500

    Ensure that DRP publication files are world-readable after archival

--- applications/digitalrocks-archive-publication/tapisjob_app.sh
+++ applications/digitalrocks-archive-publication/tapisjob_app.sh
@@ -3,12 +3,17 @@
 set -x
 
 pushd ${publishedRootDir}
+
+# Ensure that the published files are world-readable
+chmod -R 755 ${projectId}
+
 mkdir -p archive/${projectId}
 zip -r archive/${projectId}/${projectId}_archive.zip ${projectId}
 
 # Move to archive folder to add metadata JSON at the top level of the archive
 pushd archive/${projectId}
 zip -u ${projectId}_archive.zip ${projectId}_metadata.json \
+chmod 755 ${projectId}_archive.zip
 
 popd
 popd
