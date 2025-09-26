3bc94b009e9c03dda84bdf670ff867594d8a43d9
Frank <fnetscher@tacc.utexas.edu>
2025-09-26 12:09:40 -0500

    Initial commit of working paraview-vista

---
+++ applications/paraview-vista/Readme.md
@@ -0,0 +1,1 @@
+ParaView-Vista is an interactive open-source, multi-platform data analysis and visualization application, on Vista.
---
+++ applications/paraview-vista/paraview.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+module load gcc/15.1.0
+module load cuda/12.9
+module load openmpi/5.0.8
+module load paraview/5.13.3
+
+paraview
