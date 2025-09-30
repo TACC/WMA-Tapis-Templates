0a86dc8a97bfaa91fa0a06fbb0f3064a6391325d
Frank <fnetscher@tacc.utexas.edu>
2025-09-30 14:50:12 -0500

    ls-dyna testcase files

---
+++ applications/LS-Dyna-stampede3/testcase/output/parallel/simulation_constrained.linear.plate2/exec-gdb.sh
@@ -0,0 +1,2 @@
+ssh -x c455-071.stampede3.tacc.utexas.edu /scratch/05598/fnetsch/tapis/043b7b6f-fc35-4194-9229-8778dce82da5-007/inputDirectory/simulation_constrained.linear.plate2/exec-gdb-c455-071.stampede3.tacc.utexas.edu.sh
+
---
+++ applications/LS-Dyna-stampede3/testcase/output/serial/simulation_constrained.linear.plate1/exec-gdb.sh
@@ -0,0 +1,2 @@
+ssh -x c454-093.stampede3.tacc.utexas.edu /scratch/05598/fnetsch/tapis/355e28de-0c4e-447d-8551-63c312fa6c7c-007/inputDirectory/simulation_constrained.linear.plate1/exec-gdb-c454-093.stampede3.tacc.utexas.edu.sh
+
