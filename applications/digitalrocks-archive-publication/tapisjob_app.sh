05fb097ac0448797c07450156c80cbeefa47e6ef
Van Go <35277477+van-go@users.noreply.github.com>
2026-06-26 11:46:38 -0500

    Update DPMP Backup Job with Checksums

--- applications/digitalrocks-archive-publication/tapisjob_app.sh
+++ applications/digitalrocks-archive-publication/tapisjob_app.sh
@@ -8,10 +8,12 @@ pushd ${publishedRootDir}
 chmod -R 755 ${projectId}
 
 mkdir -p archive/${projectId}
+find ${projectId} -type f -print0 | xargs -0 sha512sum > archive/${projectId}/manifest-sha512.txt
 zip -r archive/${projectId}/${projectId}_archive.zip ${projectId}
 
-# Move to archive folder to add metadata JSON at the top level of the archive
+# Move to archive folder to add manifest and metadata JSON at the top level of the archive
 pushd archive/${projectId}
+zip -u ${projectId}_archive.zip manifest-sha512.txt
 zip -u ${projectId}_archive.zip ${projectId}_metadata.json
 chmod -R 755 ${projectId}_archive.zip
 
